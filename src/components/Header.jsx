9badcd3a4fc6853f3a2c09a9e5679e2aa4b528de
Curtis Witter <inward-panting0f@icloud.com>
2025-02-07 12:11:26 +0000

    Working page links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
         <>
         <header>
             <h1>App title for all pages</h1> //!Change me
+            <nav>
+                <Link href={"/"}>Home</Link>
+                <Link href={"/about"}>About</Link>
+            </nav>
         </header>
         </>
     )
