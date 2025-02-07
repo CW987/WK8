47b40b4c95dae65138c2940ca0a15ea176becdfd
Curtis Witter <inward-panting0f@icloud.com>
2025-02-07 19:23:03 +0000

    Basic working query

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,13 +1,23 @@
-export default async function usersPage() {
+import Link from "next/link";
+
+export default async function usersPage({ searchParams }) {
+    
     const usersData = await fetch(`https://dummyjson.com/users`);
     console.log("fetch complete");
     const parsedData = await usersData.json();
-    console.log("parsing")
+    console.log("parsing");
     const wrangledData = parsedData.users;
-    console.log(wrangledData)
+    console.log(wrangledData);
+
+    const query = await searchParams;
+    if(query.sort === "desc") {
+        wrangledData.reverse();
+    }
     return (
         <>
         <h1>Users Page General</h1>
+        <Link href={`/users?sort=asc`} >Ascending</Link>
+        <Link href={`/users?sort=desc`}>Descending</Link>
         {
             wrangledData.map((user) => (
             <div key={user.id}>
