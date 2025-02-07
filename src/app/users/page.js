310f29747dce74ef27675a793e80f102b24af884
Curtis Witter <inward-panting0f@icloud.com>
2025-02-07 15:25:03 +0000

    data wrangling skeleton

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,9 +1,21 @@
 export default async function usersPage() {
     const usersData = await fetch(`https://dummyjson.com/users`);
-    console.log(usersData);
+    console.log("fetch complete");
     const parsedData = await usersData.json();
-    console.log(parsedData)
+    console.log("parsing")
+    const wrangledData = parsedData.users;
+    console.log(wrangledData)
     return (
+        <>
         <h1>Users Page General</h1>
+        {
+            wrangledData.map((user) => (
+            <div key={user.id}>
+                <h2>{user.firstName} {user.lastName}</h2>
+                <p>{user.age}</p>
+            </div>
+            ))
+        }
+        </>
     )
 }
