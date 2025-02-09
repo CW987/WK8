ba3689728e5c259d1dc17dfba70817cecd68dcd6
Curtis Witter <inward-panting0f@icloud.com>
2025-02-09 15:04:21 +0000

    Database returning data, wrangled and displayed

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,26 @@
-import { db } from "@/utils/dbConnection"
+import { db } from "@/utils/dbConnection";
 
-export default function Home() {
-  
-  return (
-    <>
-    <h1>Wolves</h1>
-    {}
-    </>
-  );
+export default async function Home() {
+  try {
+    const result = await db.query(`SELECT * FROM "blogposts"`);
+    console.log("Supabase response:", result);
+    const wrangledblogposts = result.rows;
+    console.log(wrangledblogposts)
+
+    return (
+      <main>
+        <h1>Wolves</h1>
+          {wrangledblogposts.map((post) => (
+            <div key={post.id}>
+              <h2>{post.title}</h2>
+              <h3> {post.author} </h3>
+              <p> {post.blogpost} </p>
+            </div>
+          ))}
+      </main>
+    );
+  } catch (error) {
+    console.error("Database query failed:", error);
+    return <main><h1>Error fetching data</h1><p>{error.message}</p></main>;
+  }
 }
